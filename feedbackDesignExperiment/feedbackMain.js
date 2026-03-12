628caae0cccf1aa16f5e55ed0767459f094f94cb
Patrick McMahon <patrick.mcmahon@rmit.edu.au>
2026-03-12 13:19:22 +1100

    feedback start

---
+++ feedbackDesignExperiment/feedbackMain.js
@@ -0,0 +1,49 @@
+//// FIND ELEMENTS
+const introDialog = document.getElementById("intro-dialog");
+const dialogCloseButton = document.getElementById("dialog-close-button");
+const playButton = document.getElementById("play-button");
+
+//// TONE INIT
+// set up our basic synth
+const synth = new Tone.Synth().toDestination();
+
+// run after user interaction
+async function toneInit(){
+    await Tone.start();
+    console.log("close");
+}
+
+//// DIALOG INIT
+// show modal on page load
+introDialog.showModal();
+
+dialogCloseButton.addEventListener("click", () => {
+    introDialog.close();
+});
+
+introDialog.addEventListener("close", toneInit);
+
+//// SOUND FUNCTIONS
+
+function playNote(){
+    // trigger C4 eighth note on synth
+    synth.triggerAttackRelease("C4", "8N");
+}
+
+//playButton.addEventListener("click", playNote);
+
+function noteOn() {
+    synth.triggerAttack("C4", Tone.now());
+}
+
+playButton.addEventListener("mousedown", () => {
+    noteOn();
+});
+
+function noteOff() {
+    synth.triggerRelease();
+}
+
+playButton.addEventListener("mouseup", () => {
+    noteOff();
+});
